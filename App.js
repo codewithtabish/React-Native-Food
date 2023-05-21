db602033a306cbab3357e2b820ab2e4cacbda260
DESKTOP-NPUVJIQ\Mark 1 <tabish169000@gmail.com>
2023-05-21 08:09:13 -0700

    configuration of tailwind css

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="bg-green-600 text-white">Open up App.js to start working on your app!</Text>
+      <Text className="bg-red-600 text-white">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
